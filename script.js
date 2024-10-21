429d983ca1a2cc281e5b4ae90bc5b81c2c58ae2f
CitlaliSC <citlaserratos22@gmail.com>
2024-10-21 07:14:00 -0600

    add-section & list-section done

---
+++ script.js
@@ -0,0 +1,38 @@
+var inpTask = document.getElementById("inpTask");
+var btnAdd = document.getElementById("btnAdd");
+var list = document.getElementById("list");
+
+var total = 1;
+
+btnAdd.onclick = function() {
+    var task = inpTask.value;
+
+    if (task === "") {
+        alert("El campo de la tarea no debe quedar vacío.");
+    } else {
+        var tr = document.createElement("tr");
+    
+        var tdi = document.createElement("td");
+        tdi.textContent = total;
+        tr.appendChild(tdi);
+    
+        var tdTask = document.createElement("td");
+        tdTask.textContent = task;
+        tr.appendChild(tdTask);
+    
+        var tdBtn = document.createElement("td");
+        var btnDelete = document.createElement("button");
+        btnDelete.type = "button";
+        btnDelete.innerHTML = 'Eliminar';
+        btnDelete.onclick = function() {
+            tr.remove();
+        }
+        tdBtn.appendChild(btnDelete);
+        tr.appendChild(tdBtn);
+        list.appendChild(tr);
+    
+        total++;
+        inpTask.value = "";
+        inpTask.focus();
+    }
+}    
