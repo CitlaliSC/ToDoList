252554be8cfdb7d52f4b8e6ddcba2b2d8ce523ba
CitlaliSC <citlaserratos22@gmail.com>
2024-11-20 19:39:29 -0600

    pwa

--- sw.js
+++ sw.js
@@ -1,15 +1,45 @@
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["./", "./html/list.html", "./scripts/list_script.js", "./scripts/load_list.js", "./css/style.css", "./scripts/index.js", "./html/register.html"])
-        })
-    );
+const CACHE_NAME = "static-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./html/list.html",
+  "./scripts/list_script.js",
+  "./scripts/load_list.js",
+  "./scripts/index.js",
+  "./html/register.html",
+  "./css/style.css",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Pre-caching files...");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log("Clearing old cache:", cache);
+            return caches.delete(cache);
+          }
         })
-    );
+      );
+    })
+  );
 });
