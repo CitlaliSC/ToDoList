df1309413f438fdf8fe1f1034b62d02cb24f2c79
CitlaliSC <citlaserratos22@gmail.com>
2025-03-12 15:53:48 -0600

    Google integrated to register and login

--- scripts/log_auth.js
+++ scripts/log_auth.js
@@ -1,10 +1,27 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { firebaseConfig } from "../reusable_scripts/firebaseConfig.js";
 import { getTasksFromFirestore } from "./load_list.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const btnGoogle = document.getElementById('google-login');
+btnGoogle.addEventListener("click", async function () {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+    localStorage.setItem('userUID', user.uid);
+    
+    await getTasksFromFirestore();
+
+    window.location.href = "./html/list.html"; // Redirige a la lista de tareas
+  } catch (error) {
+    console.error("Error en la autenticación con Google:", error);
+    alert(error.message);
+  }
+});
 
 const btnLogin = document.getElementById('btnLogin');
 btnLogin.addEventListener("click", async function (event) {
--- scripts/reg_auth.js
+++ scripts/reg_auth.js
@@ -1,11 +1,31 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { firebaseConfig } from "../reusable_scripts/firebaseConfig.js";
+import { getTasksFromFirestore } from "./load_list.js";
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+
+const btnGoogle = document.getElementById('google-login');
+btnGoogle.addEventListener("click", async function () {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+    localStorage.setItem('userUID', user.uid);
+    
+    await getTasksFromFirestore();
+
+    window.location.href = "list.html"; // Redirige a la lista de tareas
+  } catch (error) {
+    console.error("Error en la autenticación con Google:", error);
+    alert(error.message);
+  }
+});
 
 const btnRegister = document.getElementById('btnRegister');
 
